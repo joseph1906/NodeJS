8798cc88e721a710ec7047ef74e6d04c3001ed1b
joseph1906 <josephsajsawasawa@gmail.com>
2025-10-24 00:44:56 +0300

    The use of URLS for pathing

--- FileSystem.js
+++ FileSystem.js
@@ -138,3 +138,22 @@ console.log(`operating System Type: ${type}`);
 
 const tmpdir = require('os').tmpdir();
 console.log(`Temporary Directory: ${tmpdir}`);
+
+let http = require('http');
+let url = require('url');
+let System = require('fs');
+
+http.createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type':'text/html'});
+            return res.end("404 Not Found");
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        return res.end(data);
+        return res.end();
+    });
+}).listen(8080);
