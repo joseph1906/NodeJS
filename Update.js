d692619c6ed58b30561e3de04e1139119e68cb9a
joseph1906 <josephsajsawasawa@gmail.com>
2026-01-27 15:50:35 +0300

    The update of the body-parser

--- Update.js
+++ Update.js
@@ -15,7 +15,7 @@ readStream.pipe(gunzip).pipe(writeStream);*/
 })
 
 server.listen('3000');*/
-const http = require('http');
+/*const http = require('http');
 const fs = require('fs');
 http.createServer((req,res) => {
     const create = fs.createWriteStream('index.html');
@@ -24,4 +24,47 @@ http.createServer((req,res) => {
     const readStream = fs.createReadStream('./index.html');
     res.writeHead(200, {'Content-type': 'text/html'});
     readStream.pipe(res);
-}).listen(3000);
+}).listen(3000);*/
+
+
+/*const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from SAJ.COM');
+});
+app.listen(3000);
+
+app.get('/SAJ', (req, res)=> {
+    res.send('Allo, allo from SAJ.COM');
+})
+app.get('/SAJ/:name/:age', (req,res)=> {
+    console.log(req.params);
+    res.send(`Hello ${req.params.name}, you are ${req.params.age}`);
+})*/
+/*const express = require('express');
+const app = express();
+const path = require('path');
+app.use('/public', express.NODEJS(path.join(__dirname,'NODEJS')));
+app.get('/', (res,req)=> {
+    res.sendFile(path.join(__dirname,NODEJS,'index.html'));
+})
+app.listen(3000);*/
+const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use('/public',express.static(path.join(__dirname,'static')));
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'static','form.html'));
+})
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.post('/', (req,res) =>{
+    console.log(req.body);
+    res.json({success:true});
+});
+app.listen(3000);
